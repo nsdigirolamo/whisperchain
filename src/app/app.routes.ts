13586e82800b550e958a738910d08e66c88989d9
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-11-08 18:00:42 -0500

    Added basic routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ChatComponent } from './chat/chat.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'chat', component: ChatComponent },
+    {path: '**', component: PageNotFoundComponent}
+];
